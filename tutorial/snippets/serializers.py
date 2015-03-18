bcb2474a0dc565c403cfaeb8dcf73334e3103e5b
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-03-17 17:17:24 -0700

    mixins

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -6,14 +6,14 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'line_nums', 'language', 'style')    
+        fields = ('id', 'title', 'code', 'line_nums', 'language', 'style')
     def update(self, instance, validated_data):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
-        instance.line_nums = validated_data.get('linenos', instance.linenos)
+        instance.line_nums = validated_data.get('line_nums', instance.line_nums)
         instance.language = validated_data.get('language', instance.language)
         instance.style = validated_data.get('style', instance.style)
         instance.save()
