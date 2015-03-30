aff65cc9c5559398904ac6eb5ab6bbfd69e99b7e
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-03-29 21:20:31 -0700

    up to: Object Level Permissions

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,15 +1,21 @@
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'line_nums', 'language', 'style')
+        fields = ('owner', 'id', 'title', 'code', 'line_nums', 'language',
+                  'style')
+
     def update(self, instance, validated_data):
         """
-        Update and return an existing `Snippet` instance, given the validated data.
+        Update and return an existing `Snippet` instance, given the
+        validated data.
         """
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
@@ -18,3 +24,13 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
